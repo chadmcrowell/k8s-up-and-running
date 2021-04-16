a142bbc7fbb0887e42c68b082cff0d1cba2a55d1
Chad Crowell <chad@ccmac.local>
2021-04-16 09:24:32 -0500

    updated main.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,23 @@
 output "id" {
-  value = azurerm_kubernetes_cluster.example.id
+  value = azurerm_kubernetes_cluster.myaks.id
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.myaks.kube_config_raw
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
+  value = azurerm_kubernetes_cluster.myaks.kube_config.0.client_key
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.myaks.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.myaks.kube_config.0.cluster_ca_certificate
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.host
+  value = azurerm_kubernetes_cluster.myaks.kube_config.0.host
 }
