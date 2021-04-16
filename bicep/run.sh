c0b7fe2b6106e9658c125b2b6e17e041c0bcf614
Chad Crowell <chad@ccmac.local>
2021-04-16 05:57:56 -0500

    updated bicep

--- bicep/run.sh
+++ bicep/run.sh
@@ -4,26 +4,13 @@ export PREFIX="bicepk8s"
 export SUFFIX="rg"
 export RG_NAME=$PREFIX-$SUFFIX
 export RG_LOCATION="eastus"
-export BICEP_FILE="000-main.bicep"
-export WEBINAR_PARAMETERS="@parameters.json"
-export SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" \
-# Must search for the 'ACRPull' role to get GUID - Bicep/ARM Templates does not have the ability to dynamically lookup permission role names and UUIDs
-export ACR_ROLE=$(az role definition list --name 'AcrPull' | jq -r .[].id)
-
-# Login to your Azure account
-# az login
+export BICEP_FILE="main.bicep"
 
 # Create the Resource Group to deploy the Webinar Environment
 az group create --name $RG_NAME --location $RG_LOCATION
 
-# Deploy
+# Deploy AKS cluster using bicep template
 az deployment group create \
-  --name webinarenvironment \
+  --name bicepk8sdeploy \
   --resource-group $RG_NAME \
-  --template-file $BICEP_FILE \
-  --parameters $WEBINAR_PARAMETERS \
-  --parameters prefix=$PREFIX \
-  --parameters suffix=$SUFFIX \
-  --parameters adminPublicKey="$SSH_KEY" \
-  --parameters acrRole=$ACR_ROLE \
-  --mode Incremental
+  --template-file $BICEP_FILE
