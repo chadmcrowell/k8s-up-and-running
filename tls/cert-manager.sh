0693044420690c8dc491e5984bbbc6eb8a68a90f
Chad Crowell <chad@ccmac.local>
2021-04-30 10:46:26 -0500

    added cert-manager resources

---
+++ tls/cert-manager.sh
@@ -0,0 +1,34 @@
+# get fqdn of pubic ip
+az network public-ip list --resource-group bicepk8s-rg --query "[?name=='RobotAppIP'].[dnsSettings.fqdn]" -o tsv
+
+# give fqdn to public ip
+az network public-ip update -g bicepk8s-rg -n RobotAppIP --dns-name robotshop --allocation-method Static
+
+# generate key
+openssl genrsa -out ca.key 2048
+
+# create certificate
+openssl req -x509 -new -nodes -key ca.key -sha256 -subj "/CN=robotshop.eastus.cloudapp.azure.com" -days 1024 -out ca.crt -extensions v3_ca -config /etc/ssl/openssl.cnf
+
+# create secret
+kubectl create secret tls rbot-tls --key=ca.key --cert=ca.crt
+
+# add repo to helm
+helm repo add jetstack https://charts.jetstack.io
+
+# update helm repos
+helm repo update
+
+# create ns
+kubectl create ns cert-manager
+
+# install cert manager
+helm install \
+cert-manager jetstack/cert-manager \
+--namespace cert-manager \
+--create-namespace \
+--version v1.3.1 \
+--set installCRDs=true
+
+# Label the lets-encrypt namespace to disable resource validation
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
